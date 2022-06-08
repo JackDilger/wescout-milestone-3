17d2f122046ebc70e0645dd60f3c1cb3ee652cd9
JackDilger <jack.dilger1995@gmail.com>
2022-06-08 18:07:23 +0000

    Add Collapsible component to players.html

--- wescout/static/js/script.js
+++ wescout/static/js/script.js
@@ -3,3 +3,8 @@ document.addEventListener("DOMContentLoaded", function() {
     let sidenav = document.querySelectorAll(".sidenav");
     M.Sidenav.init(sidenav);
 }); 
+
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.collapsible');
+    var instances = M.Collapsible.init(elems, options);
+  });
