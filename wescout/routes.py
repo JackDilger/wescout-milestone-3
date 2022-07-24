f8924d21fe3ea4dbfcd9edfc35050a77093363fd
JackDilger <jack.dilger1995@gmail.com>
2022-07-24 10:47:34 +0000

    Fix file name and content for error templates

--- wescout/routes.py
+++ wescout/routes.py
@@ -232,10 +232,10 @@ def search():
 
 
 @app.errorhandler(404)
-def page_not_found(error):
+def page_not_found(e):
     return render_template("404.html"), 404
 
 
 @app.errorhandler(500)
-def error_500(error):
+def internal_server_error(e):
     return render_template("500.html"), 500
