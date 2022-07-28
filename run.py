deb9f5ebf36a6ff4714223d42531ac26d65d38a6
JackDilger <jack.dilger1995@gmail.com>
2022-07-28 21:42:19 +0000

    Remove debug mode

--- run.py
+++ run.py
@@ -5,6 +5,5 @@ from wescout import app
 if __name__ == "__main__":
     app.run(
         host=os.environ.get("IP"),
-        port=int(os.environ.get("PORT")),
-        debug=os.environ.get("DEBUG")
+        port=int(os.environ.get("PORT"))
     )
